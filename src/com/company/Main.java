ff64620275f67dd23e3098ec9de057c46be8863f
BT_1N3_06@BKTECH.LOCAL <BT_1N3_06@BKTECH.LOCAL>
2018-10-15 08:41:57 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Person p1 = new Person("Deja", "555 132-3253");
+        System.out.println(p1);
+        Person p2 = new Person("Avery", "555 132-6632");
+        System.out.println(p2);
+    }
+}
